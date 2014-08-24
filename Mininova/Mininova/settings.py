f702fadbb582d6a338b5f31e905a2187a38d4cc1
kevin <yangjin1989@gmail.com>
2014-08-25 02:03:51 +0800

    first commit

---
+++ Mininova/Mininova/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for Mininova project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'Mininova'
+
+SPIDER_MODULES = ['Mininova.spiders']
+NEWSPIDER_MODULE = 'Mininova.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'Mininova (+http://www.yourdomain.com)'
